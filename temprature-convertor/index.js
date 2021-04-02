22bff7c74eb7505eade710b9dab7e241f1e09470
PD606 <praveenh.cs17@sahyadri.edu.in>
2021-04-02 15:26:50 +0530

    added temp-convertor

--- temprature-convertor/index.js
+++ temprature-convertor/index.js
@@ -2,14 +2,37 @@ function convert(deg){
     
     var f =document.getElementById('fahrenheit');
     var c =document.getElementById('celsius');
+    var k = document.getElementById('kelvin');
         
     if(deg=='c'){
         cel = (f.value-32)*(5/9);
         c.value=cel;
+
+        kel = (5/9) * (f.value - 32) + 273.15
+
+        k.value = kel;
+
     }
             
     if(deg=='f'){
         far  = c.value * (9/5) +32;
-        f.value=far;
+        f.value=(far);
+
+        kel = c.value + 273.15
+        k.value = kel;
     }
+
+    if(deg=='k'){
+        
+        far = 1.8*(k.value-273) + 32
+        cel = k.value - 273.15
+
+        console.log(far);
+
+        f.value = far;
+        c.value = cel;
+
+    }
+
+
 }
